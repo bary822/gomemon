f353fb96b78c85227cc9a54cc5d5f97365210c4b
bary822 <hiroto@hey.com>
2023-04-04 11:53:57 +0900

    Extract and hide template out of main function

--- main.go
+++ main.go
@@ -4,15 +4,27 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// Hello world, the web server
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "<h1>Hello World!!</h1>")
+		io.WriteString(w, ReadShowTemplate("hello"))
 	}
 
 	http.HandleFunc("/hello", helloHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ReadShowTemplate(resource string) string {
+	html, err := os.ReadFile("templates/" + resource + "/show.html")
+
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	return string(html)
 }
