162bd2f127ead28a49039cd3dae4b7356f7ae14c
bary822 <hiroto@hey.com>
2023-04-04 19:43:18 +0900

    You can now dynamically view various memos

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"io"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/memo", memoHandler)
+	http.HandleFunc("/memo/", memoHandler)
 
 	http.HandleFunc("/", notFoundHandler)
 
@@ -16,21 +18,71 @@ func main() {
 }
 
 func memoHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, readTemplate("memo", "show"))
-}
-
-func notFoundHandler(w http.ResponseWriter, req *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	io.WriteString(w, readTemplate("error", "404"))
-}
-
-func readTemplate(resource string, action string) string {
-	html, err := os.ReadFile("templates/" + resource + "/" + action + ".html")
-
+	memoID, err := strconv.Atoi(strings.Split(req.URL.Path, "/")[2])
 	if err != nil {
 		log.Fatal(err)
 		panic(err)
 	}
 
-	return string(html)
+	memo, err := fetchMemoById(MemoID(memoID))
+	if err != nil {
+		log.Println(err)
+		notFoundHandler(w, req)
+		return
+	}
+
+	readTemplate("memo", "show").ExecuteTemplate(w, "show.html", struct {
+		Title   string
+		Content string
+	}{
+		Title:   memo.Title,
+		Content: memo.Content,
+	})
+}
+
+type MemoID int
+
+type Memo struct {
+	ID      MemoID
+	Title   string
+	Content string
+}
+
+func fetchMemoById(memoID MemoID) (Memo, error) {
+	memoFixtures := []Memo{
+		{
+			ID:      1,
+			Title:   "Grocery List",
+			Content: "Milk, Bread, Eggs",
+		},
+		{
+			ID:      2,
+			Title:   "Meeting Notes",
+			Content: "Reviewed project timeline and goals",
+		},
+		{
+			ID:      3,
+			Title:   "Vacation Ideas",
+			Content: "Beach trip to Hawaii or ski trip to Aspen",
+		},
+	}
+
+	for _, memo := range memoFixtures {
+		if memo.ID == memoID {
+			return memo, nil
+		}
+	}
+
+	return Memo{}, fmt.Errorf("Memo with ID %d not found", memoID)
+}
+
+func notFoundHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	readTemplate("error", "404").Execute(w, nil)
+}
+
+func readTemplate(resource string, action string) *template.Template {
+	t := template.Must(template.ParseFiles("templates/" + resource + "/" + action + ".html"))
+
+	return t
 }
