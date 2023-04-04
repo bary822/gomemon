6f5e36c55809b10e071f0b7489d893b418353820
bary822 <hiroto@hey.com>
2023-04-04 15:26:17 +0900

    Respond with custom 404 error page when no route is matched

--- main.go
+++ main.go
@@ -10,15 +10,22 @@ import (
 func main() {
 	http.HandleFunc("/memo", MemoHandler)
 
+	http.HandleFunc("/", NotFoundHandler)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func MemoHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, ReadShowTemplate("memo"))
+	io.WriteString(w, ReadTemplate("memo", "show"))
 }
 
-func ReadShowTemplate(resource string) string {
-	html, err := os.ReadFile("templates/" + resource + "/show.html")
+func NotFoundHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	io.WriteString(w, ReadTemplate("error", "404"))
+}
+
+func ReadTemplate(resource string, action string) string {
+	html, err := os.ReadFile("templates/" + resource + "/" + action + ".html")
 
 	if err != nil {
 		log.Fatal(err)
